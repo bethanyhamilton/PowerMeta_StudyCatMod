29de8c6a5b24c24926e3c6e7e563eb10c53efd9f
bethanyhamilton <bethany.hamilton09@gmail.com>
2025-03-07 13:22:47 -0600

    A few edits to the TACCestimates.R

--- SimFunctions/TACCestimates.R
+++ SimFunctions/TACCestimates.R
@@ -14,8 +14,20 @@ design_factors <- list(
   rho = c(.2,  .8),
   P = c(0.05, 0.2, 0.4, 0.6, 0.8, 0.9),
   f_c_val = c("P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8"),
-  bal = c("balanced_j", "unbalanced_j")
+  bal = c("balanced_j", "unbalanced_j") #,
+#  batch_id = c("batch1","batch2") ### add this in run figure out new time for each batch when dividing 2500 replications across batches 
 )
+
+#run smaller batches of replications. set iterations to smaller so each row runs faster.
+#instead of running in parallel we will run one condition/job on one node. This way when a job finishes in a queue another job will start
+#this is more efficient than running in parallel in R.
+#First pass do just batch id 1s. 
+#maybe 1250 replications for first batch might be good -- **** 
+
+# use pylaunch to run the everything on TACC
+
+#save raw files
+
 
 
 params <- expand.grid(design_factors) %>% 
@@ -49,7 +61,7 @@ set.seed(0303202571)
 # 
 # saveRDS(results, tm, "SimFunctions/TACCtest.rds")
 
-dat_kjN <- readRDS("SimFunctions/TACCtest.rds")
+results <- readRDS("SimFunctions/TACCtest.rds")
 
 tm_j<- results |> 
   group_by(J) |> 
@@ -63,4 +75,17 @@ tm_j
 as.numeric(sum(tm_j$tot_tm_byJ))/60^2
 
 
-  
+test <- results |> group_by(J, tau_sq, omega_sq, rho, P, f_c_val, bal) |>      summarise(
+        n_sim = n(),
+        cnvg = mean(!is.na(p_val)),
+      #  df1_mean = mean(df1),
+      #  df2_mean = mean(df2),
+        rej_rate_05 = mean(p_val < 0.05, na.rm = TRUE),
+        .groups = "drop"
+      ) |> 
+  mutate(
+    diff = P - rej_rate_05
+  )
+
+
+
